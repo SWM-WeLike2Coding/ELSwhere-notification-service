61595962e98b8a14963f0fec4fbd801b46d9adde
kjungw1025 <kjungw1025@naver.com>
2024-08-30 14:01:15 +0900

    conf: kafka및 openfeign 의존성 추가

--- src/main/java/com/wl2c/elswherenotificationservice/ElSwhereNotificationServiceApplication.java
+++ src/main/java/com/wl2c/elswherenotificationservice/ElSwhereNotificationServiceApplication.java
@@ -2,8 +2,12 @@ package com.wl2c.elswherenotificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ElSwhereNotificationServiceApplication {
 
 	public static void main(String[] args) {
